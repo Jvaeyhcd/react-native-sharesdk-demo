9398e502ba89167645ec9d3438574fc5c0febd32
Jvaeyhcd <chedahuang@icloud.com>
2018-12-20 09:32:38 +0800

    share sdk demo

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, NativeModules} from 'react-native';
+const {MobLogin} = NativeModules
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          onPress={this.onPressShare}
+          title="我要分享"
+          color="#841584"></Button>
+      </View>
+    );
+  }
+
+  onPressShare() {
+    MobLogin.showShare('我是标题', '分享什么内容', 'http://sharesdk.cn', 'http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg')
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
